5712e0043ef54239eeb94707ef47412878fbf04c
Dale Roberts <dale.o.roberts@gmail.com>
2018-09-19 19:26:55 +1000

    Geomedian implementation

--- s2cloudmask/__init__.py
+++ s2cloudmask/__init__.py
@@ -6,6 +6,26 @@ from typing import Union, Optional
 Data = Union[xr.Dataset, np.array]
 
 
+try:
+    # Import the superfast version of geomedian pixel composites. 
+    # Contact Dale Roberts <dale.roberts@anu.edu.au> if you want 
+    # to discuss GitHub repo access to the development version.
+    from pcm import gmpcm
+
+except ImportError:
+    # Use the slow public version
+    from hdmedians import nangeomedian
+
+    def gmpcm(data):
+        result = np.empty(data.shape[:3], dtype=np.float32)
+        for y in range(data.shape[0]):
+            for x in range(data.shape[1]):
+                result[y, x, :] = nangeomedian(
+                    data[y, x, :, :].astype(np.float32), axis=1, eps=1e-4, maxiters=2000
+                )
+        return result
+
+
 class Classifier:
     def __init__(self):
         pass
